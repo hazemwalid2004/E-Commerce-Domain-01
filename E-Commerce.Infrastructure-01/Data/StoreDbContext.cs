337766b887c72812e94e724461a69566c768f225
Hazem walid <walidf106@gmail.com>
2026-07-28 14:52:13 +0300

    session01

---
+++ E-Commerce.Infrastructure-01/Data/StoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using E_Commerce.Domain.Entities.Products;
+
+namespace E_Commerce.Infrastructure_01.Data
+{
+    public class StoreDbContext(DbContextOptions<StoreDbContext> options):DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+        }
+    }
+}
